5bf90d81494cad01175b9c1c313fe07a71d551d7
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-04-08 13:20:11 +0600

    banner title more modify

--- src/Components/Shared/Banner.jsx
+++ src/Components/Shared/Banner.jsx
@@ -5,10 +5,10 @@ const Banner = () => {
        <div className="hero bg-base-200 min-h-[70vh] rounded-2xl my-8 container mx-auto w-[85%] px-10">
             <div className="hero-content flex-col lg:flex-row-reverse w-full justify-between">
                 <img className='h-70 w-auto'
-                    src="/assets/banner.png"
+                    src="/banner.png"
                 />
                 <div>
-                    <h1 className="text-5xl font-bold">Books to freshen up <br /> your bookshelf here</h1>
+                    <h1 className="text-3xl md:text-5xl font-bold">Books to freshen up your bookshelf are here</h1>
                     
                     <button className="btn btn-success text-white mt-8">View the List</button>
                 </div>
